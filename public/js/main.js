f47bec16023a0e8b0779a59bd7af1493391ddaa3
Roopesh <roopeshkr2003@gmail.com>
2025-02-17 20:53:41 +0530

    backend url

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const { username, room } = Qs.parse(location.search, {
   ignoreQueryPrefix: true,
 });
 
-const socket = io();
+const socket = io('https://farmer-s-community.onrender.com/');
 
 // Join chatroom
 socket.emit('joinRoom', { username, room });
